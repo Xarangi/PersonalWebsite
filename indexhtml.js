fb3aead46e5b2a4a05c19d160866bc40bd171a6a
Xarangi <sneheelsarangi@gmail.com>
2021-09-18 19:21:30 +0400

    setting up https redirect

--- indexhtml.js
+++ indexhtml.js
@@ -9,3 +9,8 @@ app.set('port', process.env.PORT || 8080);
 var server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
 });
+
+app.enable('trust proxy')
+app.use((req, res, next) => {
+    req.secure ? next() : res.redirect('https://' + req.headers.host + req.url)
+})

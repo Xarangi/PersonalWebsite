f1091fad9804c463c6711eba452fe2098b69d089
Xarangi <sneheelsarangi@gmail.com>
2021-09-18 19:30:22 +0400

    configruing redicrect

--- indexhtml.js
+++ indexhtml.js
@@ -10,7 +10,9 @@ var server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
 });
 
-app.enable('trust proxy')
 app.use((req, res, next) => {
-    req.secure ? next() : res.redirect('https://' + req.headers.host + req.url)
+  if (req.header('x-forwarded-proto') !== 'https')
+    res.redirect(`https://${req.header('host')}${req.url}`)
+  else
+    next()
 })

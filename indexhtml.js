13ecd16a1f89cb6ae8827b268dc89be561b9b77d
Xarangi <sneheelsarangi@gmail.com>
2021-09-18 19:35:39 +0400

    https redirect

--- indexhtml.js
+++ indexhtml.js
@@ -3,16 +3,16 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
-app.set('port', process.env.PORT || 8080);
-
-var server = app.listen(app.get('port'), function() {
-  console.log('listening on port ', server.address().port);
-});
-
 app.use((req, res, next) => {
   if (req.header('x-forwarded-proto') !== 'https')
     res.redirect(`https://${req.header('host')}${req.url}`)
   else
     next()
 })
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.set('port', process.env.PORT || 8080);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('listening on port ', server.address().port);
+});

57c229421e1705bfd4666968f8e36cb6f0711f00
Xarangi <sneheelsarangi@gmail.com>
2023-12-13 07:41:34 +0400

    made updates

--- src/script.js
+++ src/script.js
@@ -42,7 +42,7 @@ const geometry2 = new THREE.RingBufferGeometry(3.5,4,8);
 // const geometry4 = new THREE.RingBufferGeometry(2.9,3,8);
 // const geometry5 = new THREE.RingBufferGeometry(1.9,2,8);
 const particlesGeometry = new THREE.BufferGeometry;
-const particlesCnt = 200;
+const particlesCnt = 250;
 
 const posArray = new Float32Array(particlesCnt*3);
 const posArray1 = new Float32Array(particlesCnt);
@@ -52,7 +52,7 @@ for(let i=0; i<particlesCnt*3; i++){
 }
 for(let i=0;i<particlesCnt;i++)
 {
-    posArray1[i]= (Math.random()-0.5)%80 -20
+    posArray1[i]= (Math.random()-0.5)%80-40
 }
 
 particlesGeometry.setAttribute('position', new THREE.BufferAttribute(posArray, 3));
@@ -61,7 +61,7 @@ particlesGeometry.setAttribute('position.z', new THREE.BufferAttribute(posArray1
 // Materials
 const texture = new THREE.TextureLoader().load('texta.png');
 const texture1 = new THREE.TextureLoader().load('beg.png');
-const texture2 = new THREE.TextureLoader().load('x3.png');
+const texture2 = new THREE.TextureLoader().load('x4.png');
 //const texture3 = new THREE.TextureLoader().load('x4.png');
 const texture4 = new THREE.TextureLoader().load('x5.png');
 //const texture5 = new THREE.TextureLoader().load('x7.png');
@@ -92,20 +92,10 @@ materialz.blending = THREE.NormalBlending;
 materialz.transparent = true
 materialz.opacity=1
 
-// const materialv = new THREE.MeshBasicMaterial({map: texture3});
-// materialv.blending = THREE.NormalBlending;
-// materialv.transparent = true
-// materialv.opacity=1
-
 const materialw = new THREE.MeshBasicMaterial({map: texture4});
 materialw.blending = THREE.NormalBlending;
 materialw.transparent = true
 materialw.opacity=1
-
-// const materialy = new THREE.MeshBasicMaterial({map: texture5});
-// materialy.blending = THREE.NormalBlending;
-// materialy.transparent = true
-// materialy.opacity=1
 
 const material3 = new THREE.MeshBasicMaterial()
 material3.color = new THREE.Color(0xe85655)
@@ -186,19 +176,9 @@ octagon1.position.z=24
 octagon1.position.y=-2
 scene.add(octagon1)
 
-// const octagon2 = new THREE.Mesh(geo,materialv)
-// octagon2.position.z=29
-// octagon2.position.y=6
-// octagon2.position.x=-4
-// scene.add(octagon2)
-
 const octagon3 = new THREE.Mesh(geo,materialw)
 octagon3.position.z=29
 scene.add(octagon3)
-
-// const octagon4 = new THREE.Mesh(geo0,materialy)
-// octagon4.position.z=29
-// scene.add(octagon4)
 
 const octagon5 = new THREE.Mesh(geo,materialz)
 octagon5.position.z=43
@@ -213,11 +193,6 @@ const sphere2 = new THREE.Mesh(geoz,materialx)
 sphere2.position.z=47
 sphere2.position.y=-10
 scene.add(sphere2)
-
-// const sphere3 = new THREE.Mesh(geob,material3)
-// sphere3.position.z=47
-// sphere3.position.y=-10
-// scene.add(sphere3)
 
 const bg = new THREE.Mesh(geow,material)
 bg.position.z=46
@@ -285,10 +260,7 @@ scene.add(ring5e)
 ring5e.position.z=46.5
 ring5e.position.x=1.5
 ring5e.position.y=-32.5
-// const ring6e = new THREE.Mesh(geometry5,material2)
-// scene.add(ring6e)
-// ring6e.position.z=25
-// ring6e.position.x=13
+
 
 const ring1w = new THREE.Mesh(geometry1,material3x)
 scene.add(ring1w)
@@ -310,197 +282,6 @@ scene.add(ring4w)
 ring4w.position.z=46
 ring4w.position.x=1
 ring4w.position.y=-48
-// const ring5w = new THREE.Mesh(geometry4,material)
-// scene.add(ring5w)
-// ring5w.position.z=25
-// ring5w.position.x=-13
-// const ring6w = new THREE.Mesh(geometry5,material2)
-// scene.add(ring6w)
-// ring6w.position.z=25
-// ring6w.position.x=-13
-
-// const ring1n = new THREE.Mesh(geometry,material)
-// scene.add(ring1n)
-// ring1n.position.z=25
-// ring1n.position.y=13
-// const ring2n = new THREE.Mesh(geometry1,material2)
-// scene.add(ring2n)
-// ring2n.position.z=25
-// ring2n.position.y=13
-// const ring3n = new THREE.Mesh(geometry2,material)
-// scene.add(ring3n)
-// ring3n.position.z=25
-// ring3n.position.y=13
-// const ring4n = new THREE.Mesh(geometry3,material2)
-// scene.add(ring4n)
-// ring4n.position.z=25
-// ring4n.position.y=13
-// const ring5n = new THREE.Mesh(geometry4,material)
-// scene.add(ring5n)
-// ring5n.position.z=25
-// ring5n.position.y=13
-// const ring6n = new THREE.Mesh(geometry5,material2)
-// scene.add(ring6n)
-// ring6n.position.z=25
-// ring6n.position.y=13
-
-// const ring1s = new THREE.Mesh(geometry,material)
-// scene.add(ring1s)
-// ring1s.position.z=25
-// ring1s.position.y=-13
-// const ring2s = new THREE.Mesh(geometry1,material2)
-// scene.add(ring2s)
-// ring2s.position.z=25
-// ring2s.position.y=-13
-// const ring3s = new THREE.Mesh(geometry2,material)
-// scene.add(ring3s)
-// ring3s.position.z=25
-// ring3s.position.y=-13
-// const ring4s = new THREE.Mesh(geometry3,material2)
-// scene.add(ring4s)
-// ring4s.position.z=25
-// ring4s.position.y=-13
-// const ring5s = new THREE.Mesh(geometry4,material)
-// scene.add(ring5s)
-// ring5s.position.z=25
-// ring5s.position.y=-13
-// const ring6s = new THREE.Mesh(geometry5,material2)
-// scene.add(ring6s)
-// ring6s.position.z=25
-// ring6s.position.y=-13
-
-// const ring1en = new THREE.Mesh(geometry,material)
-// scene.add(ring1en)
-// ring1en.position.z=25
-// ring1en.position.x=6.5
-// ring1en.position.y=6.5
-// const ring2en = new THREE.Mesh(geometry1,material2)
-// scene.add(ring2en)
-// ring2en.position.x=6.5
-// ring2en.position.y=6.5
-// const ring3en = new THREE.Mesh(geometry2,material)
-// scene.add(ring3en)
-// ring3en.position.z=25
-// ring2en.position.x=6.5
-// ring3en.position.y=6.5
-// const ring4en = new THREE.Mesh(geometry3,material2)
-// scene.add(ring4en)
-// ring4en.position.z=25
-// ring4en.position.x=6.5
-// ring4en.position.y=6.5
-// const ring5en = new THREE.Mesh(geometry4,material)
-// scene.add(ring5en)
-// ring5en.position.z=25
-// ring5en.position.x=6.5
-// ring5en.position.y=6.5
-// const ring6en = new THREE.Mesh(geometry5,material2)
-// scene.add(ring6en)
-// ring6en.position.z=25
-// ring6en.position.x=6.5
-// ring6en.position.y=6.5
-
-// const ring1wn = new THREE.Mesh(geometry,material)
-// scene.add(ring1wn)
-// ring1wn.position.z=25
-// ring1wn.position.x=-6.5
-// ring1wn.position.y=6.5
-// const ring2wn = new THREE.Mesh(geometry1,material2)
-// scene.add(ring2wn)
-// ring2wn.position.z=25
-// ring2wn.position.x=-6.5
-// ring2wn.position.y=6.5
-// const ring3wn = new THREE.Mesh(geometry2,material)
-// scene.add(ring3wn)
-// ring3wn.position.z=25
-// ring3wn.position.x=-6.5
-// ring3wn.position.y=6.5
-// const ring4wn = new THREE.Mesh(geometry3,material2)
-// scene.add(ring4wn)
-// ring4wn.position.z=25
-// ring4wn.position.x=-6.5
-// ring4wn.position.y=6.5
-// const ring5wn = new THREE.Mesh(geometry4,material)
-// scene.add(ring5wn)
-// ring5wn.position.z=25
-// ring5wn.position.x=-6.5
-// ring5wn.position.y=6.5
-// const ring6wn = new THREE.Mesh(geometry5,material2)
-// scene.add(ring6wn)
-// ring6wn.position.z=25
-// ring6wn.position.x=-6.5
-// ring6wn.position.y=6.5
-
-// const ring1ws = new THREE.Mesh(geometry,material)
-// scene.add(ring1ws)
-// ring1ws.position.z=25
-// ring1ws.position.y=-6.5
-// ring1ws.position.x=-6.5
-// const ring2ws = new THREE.Mesh(geometry1,material2)
-// scene.add(ring2ws)
-// ring2ws.position.z=25
-// ring2ws.position.y=-6.5
-// ring2ws.position.x=-6.5
-// const ring3ws = new THREE.Mesh(geometry2,material)
-// scene.add(ring3ws)
-// ring3n.position.z=25
-// ring3ws.position.y=-6.5
-// ring3ws.position.x=-6.5
-// const ring4ws = new THREE.Mesh(geometry3,material2)
-// scene.add(ring4ws)
-// ring4ws.position.z=25
-// ring4ws.position.y=-6.5
-// ring4ws.position.x=-6.5
-// const ring5ws = new THREE.Mesh(geometry4,material)
-// scene.add(ring5ws)
-// ring5ws.position.z=25
-// ring5ws.position.y=-6.5
-// ring5ws.position.x=-6.5
-// const ring6ws = new THREE.Mesh(geometry5,material2)
-// scene.add(ring6ws)
-// ring6ws.position.z=25
-// ring6ws.position.y=-6.5
-// ring6ws.position.x=-6.5
-
-// const ring1se = new THREE.Mesh(geometry,material)
-// scene.add(ring1s)
-// ring1se.position.z=25
-// ring1se.position.y=-6.5
-// ring1se.position.x=6.5
-// const ring2se = new THREE.Mesh(geometry1,material2)
-// scene.add(ring2se)
-// ring2se.position.z=25
-// ring2se.position.y=-6.5
-// ring2se.position.x=6.5
-// const ring3se = new THREE.Mesh(geometry2,material)
-// scene.add(ring3se)
-// ring3se.position.z=25
-// ring3se.position.y=-6.5
-// ring3se.position.x=6.5
-// const ring4se = new THREE.Mesh(geometry3,material2)
-// scene.add(ring4se)
-// ring4se.position.z=25
-// ring4se.position.y=-6.5
-// ring4se.position.x=6.5
-// const ring5se = new THREE.Mesh(geometry4,material)
-// scene.add(ring5se)
-// ring5se.position.z=25
-// ring5se.position.y=-6.5
-// ring5se.position.x=6.5
-// const ring6se = new THREE.Mesh(geometry5,material2)
-// scene.add(ring6se)
-// ring6se.position.z=25
-// ring6se.position.y=-6.5
-// ring6se.position.x=6.5
-
-
-
-// Lights
-
-// const pointLight = new THREE.PointLight(0xffffff, 0.1)
-// pointLight.position.x = 2
-// pointLight.position.y = 3
-// pointLight.position.z = 4
-// scene.add(pointLight)
 
 /**
  * Sizes
@@ -540,22 +321,6 @@ camera.position.x = 0
 camera.position.y = 0
 camera.position.z = 7.5
 scene.add(camera)
-
-//Controls
-//  const controls = new OrbitControls(camera, canvas)
-//  controls.enableDamping = true
-
-//Import
-
-// const loaderx = new GLTFLoader()
-
-// loaderx.load(
-// 	// resource URL
-// 	'untitled.glb',
-// 	// called when the resource is loaded
-// 	function ( gltf ) {   mixer= new THREE.AnimationMixer(gltf.scene);
-//         gltf.animations.forEach((clip) => {mixer.clipAction(clip).play(); });
-//      scene.add(gltf.scene)})
 
 /**
  * Renderer
@@ -641,46 +406,14 @@ const tick = () =>
     sphere2.rotation.set(0.1*elapsedTime,0.1*elapsedTime,0.3*elapsedTime)
     particlesMesh.rotation.x=0.03*elapsedTime
     particlesMesh.rotation.y=0.03*elapsedTime
-    // octagon.rotation.z = 0.4* elapsedTime
-
-    // ring1s.rotation.z = .2 * elapsedTime
-    // ring2s.rotation.z = .3 * elapsedTime
-    // ring3s.rotation.z = .4 * elapsedTime
-    // ring4s.rotation.z = .5 * elapsedTime
-    // ring5s.rotation.z = .6 * elapsedTime
-    // ring6s.rotation.z = .7 * elapsedTime
-
-    // ring1n.rotation.z = .2 * elapsedTime
-    // ring2n.rotation.z = .3 * elapsedTime
-    // ring3n.rotation.z = .4 * elapsedTime
-    // ring4n.rotation.z = .5 * elapsedTime
-    // ring5n.rotation.z = .6 * elapsedTime
-    // ring6n.rotation.z = .7 * elapsedTime
 
     ring1e.rotation.z = .5 * elapsedTime
     ring2e.rotation.z = -.5 * elapsedTime
-    // ring3e.rotation.z = .4 * elapsedTime
-    // ring4e.rotation.z = .5 * elapsedTime
-    // ring5e.rotation.z = .6 * elapsedTime
-    // ring6e.rotation.z = .7 * elapsedTime
 
     ring1w.rotation.z = .3 * elapsedTime
     ring2w.rotation.z = .3 * elapsedTime
     ring3w.rotation.z = -.3 * elapsedTime
     ring4w.rotation.z = -.3 * elapsedTime
-    // ring5w.rotation.z = .6 * elapsedTime
-    // ring6w.rotation.z = .7 * elapsedTime
-
-    // ring1s.rotation.z = .2 * elapsedTime
-    // ring2s.rotation.z = .3 * elapsedTime
-    // ring3s.rotation.z = .4 * elapsedTime
-    // ring4s.rotation.z = .5 * elapsedTime
-    // ring5s.rotation.z = .6 * elapsedTime
-    // ring6s.rotation.z = .7 * elapsedTime
-
-
-    // Update Orbital Controls
-    // controls.update()
 
     // Render
 
